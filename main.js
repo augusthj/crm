4ddef82b52d2e2a317bdc4a88f56a70e53e73ae6
KaptenAlex <alexander.m.wilson1996@gmail.com>
2019-10-21 15:22:23 +0200

    get customers via Ajax call.

--- main.js
+++ main.js
@@ -1,32 +1,9 @@
 $(document).ready(function() {
   $.ajax({
-  url: "http://5da7897d23fa740014697829.mockapi.io/customer",
-  data: {id: 0, firstName: "", lastName: "", avatar:"", email: "", phoneNumber:"", companyName: ""},
-  success: console.log("Success"),
-  dataType: "JSON"
-})
-.done(function(data){
-  $.each(data.items, function(i, item ){
-    console.log("hej");
-    console.log(i + " " + item);
-  });
-});
-
-
-
-  /*
-  let customersJSON = $.get("http://5da7897d23fa740014697829.mockapi.io/customer", function() {
-      console.log("Loading JSON file.");
-    })
-    .done(function() {
-      console.log("JSON file loaded!");
-    })
-    .fail(function() {
-      console.log("Could not load JSON file!");
-    })
-    .always(function() {
-      console.log("Jquery GET completed.");
-    });
+    method: "GET",
+    url: "http://5da7897d23fa740014697829.mockapi.io/customer"
+  })
+  .done(function(data){
     console.log(data);
-    */
+  })
 });
