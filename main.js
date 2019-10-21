e255e3c3788cb2e6f9dd9b408aaa6789b4128c85
KaptenAlex <alexander.m.wilson1996@gmail.com>
2019-10-21 15:47:00 +0200

    Removed placeholder data with real data.

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 $(document).ready(function() {
   $.ajax({
-    method: "GET",
-    url: "http://5da7897d23fa740014697829.mockapi.io/customer"
-  })
-  .done(function(data){
-    console.log(data);
-  })
+      method: "GET",
+      url: "http://5da7897d23fa740014697829.mockapi.io/customer"
+    })
+    .done(function(data) {
+      $.each(data, function(key, value) {
+        $("#companyTable > tbody")
+          .append(
+            "<tr><td><a href='companyPage.html'>" +
+            value.companyName +
+            "</a></td><td>" +
+            value.email +
+            "</td><td>" +
+            value.phoneNumber +
+            "</td><td>" +
+            value.firstName +
+            "</td></tr>"
+          );
+      })
+    })
 });
