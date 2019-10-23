d2d690b9c7b8b014a32bc3a7ba2f7e78e9ad499e
Johan Garzon <johanprograma@hotmail.com>
2019-10-23 22:38:50 +0200

    An event shows on a DIV when clicking on a day-event

--- calender.js
+++ calender.js
@@ -1,17 +1,52 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-   let day08 = document.getElementById("day08");
+   
    let eventHere = document.getElementById("event-here");
-   day08.addEventListener("click", showEvent);
-   let texto = "Do the laundry";
+
+   let someText = "16:00 - Do the laundry";
    let newEvent;
+
+   for (let i=1; i <= 31; i++) {
+      let tempElement = document.getElementById("day" + i);
+      tempElement.addEventListener("click", showEvent);
+      console.log(tempElement);
+   }
+
+
 
    function showEvent() {
       console.log("Boton oprimido");
       newEvent = document.createElement("p");
       console.log(newEvent);
-      newEvent.innerHTML = texto;
+      newEvent.innerHTML = someText;
+      newEvent.classList.add("event");
       eventHere.appendChild(newEvent);
    }
 
 
 })
+
+
+
+/*
+
+$.ajax({
+   method: "POST",
+   url: "https://5da7897d23fa740014697829.mockapi.io/events",
+   data: { date: "2020-04-26", description: "Do something else" }
+ })
+   .done(function( msg ) {
+      console.log(msg)
+     //alert( "Data Saved: " + msg );
+   });
+
+
+
+$.ajax({
+   method: "GET",
+   url: "https://5da7897d23fa740014697829.mockapi.io/events",
+ })
+   .done(function( msg ) {
+      console.log(msg)
+     
+   });
+*/
