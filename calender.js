194f6c969cc524c7a33c6118a26dbec04066d246
Johan Garzon <johanprograma@hotmail.com>
2019-10-25 00:37:12 +0200

    Add month-day subtitle when clicking a day. / Add file "ourEvents.js"

--- calender.js
+++ calender.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 
    let eventHere = document.getElementById("event-here");
+   let dayMonth = document.getElementById("day-month");
    let newEvent;
 
    /* set addListener to all buttons*/
@@ -10,11 +11,11 @@ document.addEventListener("DOMContentLoaded", function (event) {
    }
 
    function showEvent() {
-      //todo: clear div before printing
+      let clicked = this.innerHTML; // innerHTML of the clicked button
+      showDayMonth(clicked);
+      // Clear div before printing
       clearBox(eventHere);
      
-      //console.log(newEvent);
-      let clicked = this.innerHTML; // innerHTML of the clicked button
 
       $.ajax({
          method: "GET",
@@ -37,6 +38,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
             }
          });
 
+   }
+
+   function showDayMonth(day) {
+      dayMonth.innerHTML = "October " + day;
    }
 
    function clearBox(elementID) {
@@ -116,59 +121,3 @@ function getMonthMockapiEvents() {
 }
 */
 
-/*
-   Copy and paste this on the mockapi proyect
-   in case they are deleted so we have events to
-   show on October. Paste on IDs 10-15.
-
-{
-    "id": "10",
-    "userid": 58456,
-    "custmerid": 57759,
-    "date": "2019-10-02T03:52:27.710Z",
-    "description": "11:30 - Lunch Dynamic and Patricia",
-    "content": "content 10"
-  },
-  {
-    "id": "11",
-    "userid": 42828,
-    "custmerid": 13698,
-    "date": "2019-10-21T23:31:44.991Z",
-    "description": "09:00 - Regional meeting",
-    "content": "content 11"
-  },
-  {
-    "id": "12",
-    "userid": 22357,
-    "custmerid": 78831,
-    "date": "2019-10-08T14:16:22.608Z",
-    "description": "11:00 - Lunch Dynamic",
-    "content": "content 12"
-  },
-  {
-    "id": "13",
-    "userid": 22255,
-    "custmerid": 12082,
-    "date": "2019-10-14T19:52:06.360Z",
-    "description": "09:00 - International AB meeting",
-    "content": "content 13"
-  },
-  {
-    "id": "14",
-    "userid": 78370,
-    "custmerid": 61109,
-    "date": "2019-10-11T19:08:45.732Z",
-    "description": "13:00 - Boss's Birthday",
-    "content": "content 14"
-  },
-  {
-    "id": "15",
-    "userid": 80300,
-    "custmerid": 65144,
-    "date": "2019-10-23T10:12:23.833Z",
-    "description": "09:00 - Global meeting",
-    "content": "content 15"
-  },
-
-
- */
