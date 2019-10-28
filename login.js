fe1d039719c3ca07e300969d74b9a0b36277304b
yde <philip.ydehed@gmail.com>
2019-10-28 14:45:43 +0100

    login function

---
+++ login.js
@@ -0,0 +1,26 @@
+function validate() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if (username != "Micke" || password != "Emmio") {
+        
+
+        if (username != "Micke"){
+            alert("Invalid username");
+        }
+        else if (password != "Emmio"){
+            alert("Invalid password");
+        }
+        
+    }
+    else {
+        if (username == "Micke" && password == "Emmio")
+        alert("Login successfully");
+        window.location.href = "costumers.html";
+
+    }
+}
+
+
+
+
+
