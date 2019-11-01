efda8017be0ffd7160fd69d3e62bcd33f7c5ca92
KaptenAlex <alexander.m.wilson1996@gmail.com>
2019-11-01 20:28:55 +0100

    fixed spelling for all customer related files.

--- scripts/login.js
+++ scripts/login.js
@@ -2,7 +2,7 @@ function validate() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
     if (username != "Micke" || password != "Emmio") {
-        
+
 
         if (username != "Micke"){
             alert("Invalid username");
@@ -10,17 +10,12 @@ function validate() {
         else if (password != "Emmio"){
             alert("Invalid password");
         }
-        
+
     }
     else {
         if (username == "Micke" && password == "Emmio")
         alert("Login successfully");
-        window.location.href = "costumers.html";
+        window.location.href = "customers.html";
 
     }
 }
-
-
-
-
-
